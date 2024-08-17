3864fd9926315fc631481079d288a877917fe004
mlrprananta <lukeprananta@gmail.com>
2024-08-17 21:26:37 +0200

    Remove vertical padding

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,14 +4,14 @@ import Frame from "../components/Frame.tsx";
 export default function Home() {
   return (
     <div class="min-h-screen bg-purple-200 text-purple-900">
-      <div class="px-4 py-4 mx-auto">
+      <div class="px-4 mx-auto">
         <div class="max-w-screen-lg mx-auto flex flex-col items-center justify-center">
           <Temperature />
           <div class="w-full grid md:grid-cols-2 gap-2">
             <Frame source="https://www.youtube.com/embed/PyNuRpZOtAY?si=fP3-ekTVRJkRTNJw" />
             <Frame source="https://www.youtube.com/embed/Gd9d4q6WvUY?si=FpBGh9vBc4znKWun" />
-            <Frame source="https://www.youtube.com/embed/1phWWCgzXgM?si=46mUE4sUY-ITKMsa" />
             <Frame source="https://www.youtube.com/embed/zvx4y0BL50Q?si=BDgI8sWCJY8iuaeh" />
+            <Frame source="https://www.youtube.com/embed/1phWWCgzXgM?si=46mUE4sUY-ITKMsa" />
           </div>
         </div>
       </div>

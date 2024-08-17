83f56c9b129f96048950723212b8aef9266a3e3d
mlrprananta <lukeprananta@gmail.com>
2024-08-17 21:22:32 +0200

    Add more livestreams

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -1,14 +1,16 @@
 import { JSX } from "preact/jsx-runtime";
 
 type Props = {
-    source: string
-} & JSX.HTMLAttributes<HTMLIFrameElement>
+  source: string;
+} & JSX.HTMLAttributes<HTMLIFrameElement>;
 
 export default function Frame(props: Props) {
-    return <iframe
-    {...props}
-    className="aspect-video rounded-md w-full"
-    src={props.source + '&autoplay=1&mute=1'}
-    allow="autoplay"
-  />
+  return (
+    <iframe
+      {...props}
+      className="aspect-video rounded-md w-full"
+      src={props.source + "&autoplay=1&mute=1"}
+      allow="autoplay"
+    />
+  );
 }
--- src/islands/Temperature.tsx
+++ src/islands/Temperature.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
 
-const API_URL = 'https://polite-seal-20.deno.dev/temperatures';
+const API_URL = "https://polite-seal-20.deno.dev/temperatures";
 
 export default function Temperature() {
   const [temperature, setTemperature] = useState<number | null>(null);
@@ -9,35 +9,39 @@ export default function Temperature() {
     const fetchTemperature = async (position: GeolocationPosition) => {
       const { latitude, longitude } = position.coords;
       try {
-        const response = await fetch(`${API_URL}?lat=${latitude}&lon=${longitude}`);
+        const response = await fetch(
+          `${API_URL}?lat=${latitude}&lon=${longitude}`,
+        );
         const data = await response.json();
         setTemperature(data.temperature);
       } catch (error) {
-        console.error('Error fetching temperature:', error);
+        console.error("Error fetching temperature:", error);
       }
     };
 
     const handleError = (error: GeolocationPositionError) => {
-      console.error('Error getting user location:', error);
+      console.error("Error getting user location:", error);
     };
 
     navigator.geolocation.getCurrentPosition(fetchTemperature, handleError);
   }, []);
 
   const getWeatherEmoji = (temp: number | null) => {
-    if (temp === null) return '👋';
-    if (temp <= 0) return '❄️';
-    if (temp <= 10) return '🥶';
-    if (temp <= 20) return '😎';
-    if (temp <= 30) return '☀️';
-    return '🔥';
+    if (temp === null) return "👋";
+    if (temp <= 0) return "❄️";
+    if (temp <= 10) return "🥶";
+    if (temp <= 20) return "😎";
+    if (temp <= 30) return "☀️";
+    return "🔥";
   };
 
   const emoji = getWeatherEmoji(temperature);
- 
+
   return (
     <div class="flex items-center gap-2 my-4 text-2xl">
-      {temperature !== null ? `${temperature.toFixed(1)}°C` : 'Have a nice day!'}
+      {temperature !== null
+        ? `${temperature.toFixed(1)}°C`
+        : "Have a nice day!"}
       <span>{emoji}</span>
     </div>
   );

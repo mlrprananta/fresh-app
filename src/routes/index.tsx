6e7a43f7cb416a6027d7640b24406d3f068656af
mlrprananta <lukeprananta@gmail.com>
2024-09-21 11:46:59 +0200

    bugfix: remove dead livestream

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,7 +8,6 @@ export default function Home() {
         <div class="max-w-screen-lg mx-auto flex flex-col items-center justify-center">
           <Temperature />
           <div class="w-full grid md:grid-cols-2 gap-2">
-            <Frame source="https://www.youtube.com/embed/PyNuRpZOtAY?si=fP3-ekTVRJkRTNJw" />
             <Frame source="https://www.youtube.com/embed/Gd9d4q6WvUY?si=FpBGh9vBc4znKWun" />
             <Frame source="https://www.youtube.com/embed/zvx4y0BL50Q?si=BDgI8sWCJY8iuaeh" />
             <Frame source="https://www.youtube.com/embed/1phWWCgzXgM?si=46mUE4sUY-ITKMsa" />
